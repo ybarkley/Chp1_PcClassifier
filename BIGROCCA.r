10063f016f2b70b32c50605442fbbc6bc6a3cea3
ybarkley <ybarkley@hawaii.edu>
2018-08-22 10:07:01 -1000

    Latest versions, PMN and PM made almost identical.

--- BIGROCCA.r
+++ BIGROCCA.r
@@ -9,7 +9,7 @@ setwd('C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles
 # R1 <- read.csv('C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\concatenate\\BIG ROCCA FILE towed RAW.csv')
 # R2 <- read.csv('C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\concatenate\\PcMHIPEL_OldWhistles_MasterList_TowedOnly.csv')
 
-
+#####USE THIS 
 ### THIS WORKS ONLY IF ALL FILES HAVE THE SAME COLUMN NAMES
 
 my_files = list.files(pattern="*.csv$")# Gather the csv files in the directory
@@ -22,8 +22,11 @@ for (i in seq_along(temp)){
 #Combine all ROCCA files from my_data list into one file
 big_ROCCA = do.call(what = rbind.data.frame, args = my_data)
 
+write.csv(big_ROCCA, 'C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\concatenate/BigRoccaFile_PMN_TowedAll_RAW_20180816.csv', row.names=F)
+
 write.csv(big_ROCCA, 'C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\ROCCA Towed Data/BigRoccaFile_PMN_TowedAll_RAW_20180809.csv', row.names=F)
 
+#######################
 
 #OR this one works to add csv files into a list. then one function can be applied to the dataframe list as a whole
 temp = list.files(pattern="*.csv")
@@ -42,12 +45,10 @@ big_ROCCA = do.call(what = rbind.data.frame, args = my_data)
 write.csv(big_ROCCA, 'C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\BIG ROCCA FILE towed.csv', row.names=F)
 
 
-
-
 ##Load separate dataframes
 # If csv files need to be loaded as separate dataframes (separate objects) in the glob env
 # Example: Combining different ROCCA files from YB, PR, and EJ for single encounter, MHI28
-filenames <- list.files(path="C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\Raven Data\\MHI28\\concatenate", 
+filenames <- list.files(path="C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\Raven Data\\concatenate", 
                         pattern=".*csv")
 
 #Pulls out file name of csv from first character to X character
@@ -55,14 +56,14 @@ names <-substr(filenames, 1, 29)
 
 #Load csvs
 for(i in names){
-  filepath <- file.path("C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\Raven Data\\MHI28\\concatenate" ,paste(i,".csv",sep=""))
+  filepath <- file.path("C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\Raven Data\\concatenate" ,paste(i,".csv",sep=""))
   assign(i, read.csv(filepath, header=F))  ###MAKE HEADER 'F' IN CASE THERE ARE MORE COLUMNS THAN COLUMN NAMES!!!
 }
 
 #Bind the different dataframes and save as one file
 do.call(smartbind, my_data)
 bindIT<-smartbind(temp)
-write.csv(bindIT, 'C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\ROCCA Data\\PcMHI28_Enc06_20101211_RoccaContourStats_ALL.csv', row.names=F)
+write.csv(bindIT, 'C:\\Users\\Yvonne\\Documents\\PHD\\CHP1-FKW\\data\\2017- 100 New Whistles\\concatenate\\PcNWHI15.csv', row.names=F)
 
 ###
 
